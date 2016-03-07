487d00d6880906ecfc172a6ddc951f89d8ac4f98
Roger Liu <rogliu26@gmail.com>
2016-03-06 20:55:36 -0500

    added in the bare bones for dialog scripting, going to add dialog boxes next

---
+++ Underwater/Assets/Scripts/Player/States/DialogState.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogState : State<Player>
+{
+    private Player player;
+    //public DialogBox dialogBox;
+
+    public TextAsset textFile;
+
+    private string[] dialog;
+    private int dialogCounter;
+
+    string[] separatingStrings = { "\n" };
+
+    public DialogState(Player playerInstance, StateMachine<Player> fsm, TextAsset textFile)
+        : base(playerInstance, fsm)
+    {
+        player = playerInstance;
+
+        this.dialog = textFile.text.Split(separatingStrings, System.StringSplitOptions.RemoveEmptyEntries);
+        this.dialogCounter = 0;
+
+        Debug.Log(dialog[dialogCounter]);
+        //dialogBox.displayDialog(name, dialog[dialogCounter]);
+        dialogCounter++;
+    }
+
+    override public void Enter()
+    {
+    }
+
+    override public void Execute()
+    {
+        if (Controls.JumpInputDown()) //&& dialogBox.dialogField.text == dialog[dialogCounter-1])
+        {
+            Debug.Log(dialog[dialogCounter]);
+            dialogCounter++;
+        }
+
+        if (dialogCounter == dialog.Length)
+        {
+            player.ActionFsm.ChangeState(new IdleState(player, player.ActionFsm));
+        }
+    }
+
+    override public void FixedExecute()
+    {
+    }
+
+    override public void Exit()
+    {
+    }
+}
--- Underwater/Assets/Scripts/Powerups/BombPowerup.cs
+++ Underwater/Assets/Scripts/Powerups/BombPowerup.cs
@@ -5,6 +5,8 @@ public class BombPowerup : MonoBehaviour {
 
     public Weapon TimedBomber;
 
+    public TextAsset textFile;
+
     void OnTriggerEnter2D(Collider2D col)
     {
         TestHurtbox hurtbox = col.gameObject.GetComponent<TestHurtbox>();
@@ -15,6 +17,7 @@ public class BombPowerup : MonoBehaviour {
             player.AddWeapon(TimedBomber);
 
             //Make the player go into a dialog state + cutscenes I guess
+            player.ActionFsm.ChangeState(new DialogState(player, player.ActionFsm, textFile));
             Destroy(this.gameObject);
         }
     }
