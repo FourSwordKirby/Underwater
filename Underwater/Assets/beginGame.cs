9d0f95478c3867340c08371e7196ae4ed6da2cb2
Your Name <you@example.com>
2016-03-15 16:04:32 -0400

    scene transition help

--- Underwater/Assets/beginGame.cs
+++ Underwater/Assets/beginGame.cs
@@ -2,18 +2,14 @@
 using System.Collections;
 
 public class beginGame : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update() {
 		if (Input.GetKeyDown ("z")) {
 			print ("z key was pressed");
-			Application.LoadLevel ("Submarine");
-		}
+			Application.LoadLevel("Submarine");
 
+		}
 	}
+		
+
 }
---
+++ Underwater/Assets/changeLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeLevel : MonoBehaviour {
+
+
+	public void change(){
+		//float fadeTime = GameObjec.find (controler).GetComponent<Fading> ().BeginFade (1);
+		//yield return new WaitForSeconds (fadeTime);
+		//Application.LoadLevel ("Submarine");
+	}
+}
