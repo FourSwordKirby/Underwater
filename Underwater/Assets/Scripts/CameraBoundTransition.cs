415c829808bf0c4e9cd600d2f991fade72916128
Roger Liu <rogliu26@gmail.com>
2016-03-16 11:01:53 -0400

    moved around some files

---
+++ Underwater/Assets/Scripts/CameraBoundTransition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBoundTransition : MonoBehaviour {
+
+    /*self-references*/
+    public Collider2D newCameraBounds;
+
+    void OnTriggerEnter2D()
+    {
+        Camera myCamera = Camera.main;
+    }
+}
