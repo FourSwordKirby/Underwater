933d54ef205ff16b4bbef370dd8030c4b848164b
Roger Liu <rogliu26@gmail.com>
2016-03-17 17:55:29 -0400

    added in the fading out wall component

---
+++ Underwater/Assets/Scripts/DarknessScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DarknessScript : MonoBehaviour {
+
+    public float transparency = 1.0f;
+    private bool entered;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (entered)
+        {
+            if (transparency > 0.0)
+            {
+                transparency -= 0.02f;
+                Color oldColor = GetComponent<SpriteRenderer>().color;
+                Color newColor = new Color(oldColor.r, oldColor.b, oldColor.g, transparency);
+                GetComponent<SpriteRenderer>().color = newColor;
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+	}
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.GetComponent<PlayerHurtbox>() != null)
+        {
+            entered = true;
+        }
+    }
+}
