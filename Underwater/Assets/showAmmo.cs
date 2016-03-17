c03a3eb8187a0ab34b10b8f95f7b28d0e9fd2529
Your Name <you@example.com>
2016-03-16 21:10:40 -0400

    weights toggle

---
+++ Underwater/Assets/showAmmo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class showAmmo : MonoBehaviour {
+
+	public Text ammoNum;
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.activeWeapon != null) {
+			ammoNum.text = player.activeWeapon.GetComponent<Weapon> ().GetAmmoCount ().ToString();
+		} else
+			ammoNum.text = "";
+	
+	}
+}
---
+++ Underwater/Assets/weightsCheck.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class weightsCheck : MonoBehaviour {
+
+	public Player player;
+	//public Sprite weightsImage;
+	public Image weightsImage;
+	public Sprite weight; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (player.hasWeights == true) {
+			weightsImage.sprite = weight;
+		
+		}
+		if (!player.isWeighted) {
+			weightsImage.color = new Color (weightsImage.color.r, weightsImage.color.b, weightsImage.color.g, .5f);
+		} else
+			weightsImage.color = new Color (weightsImage.color.r, weightsImage.color.b, weightsImage.color.g, 1.0f);
+	}
+}
