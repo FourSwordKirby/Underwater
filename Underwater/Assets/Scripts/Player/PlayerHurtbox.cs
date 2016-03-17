d768777cf4181a381e87e594be5ee7d69b220acf
Roger Liu <rogliu26@gmail.com>
2016-03-17 18:47:00 -0400

    added in button stuff

--- Underwater/Assets/Scripts/Player/PlayerHurtbox.cs
+++ Underwater/Assets/Scripts/Player/PlayerHurtbox.cs
@@ -12,12 +12,22 @@ public class PlayerHurtbox : Hurtbox {
 
     override public void TakeHit(float hitlag, float hitstun, Vector2 knockback)
     {
+        Debug.Log("ow");
+
+        hitstun = 0.5f;
+        knockback = Vector2.up * 2.0f;
+
+        
+        owner.ActionFsm.ChangeState(new HitState(owner, hitlag, hitstun, knockback, owner.ActionFsm));
+
+        /*
         if(hitstun > 0)
             owner.ActionFsm.ChangeState(new HitState(owner, hitlag, hitstun, knockback, owner.ActionFsm));
         else
             owner.selfBody.velocity = knockback;
 
         Camera.main.GetComponent<CameraControls>().Shake();
+         */
     }
 
     override public void ApplyEffect(Parameters.DamageEffect effect)
