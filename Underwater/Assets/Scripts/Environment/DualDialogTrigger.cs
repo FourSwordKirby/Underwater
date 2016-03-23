5ae56155e6f047682027da44e64058185bda4e64
Roger Liu <rogliu26@gmail.com>
2016-03-23 11:32:42 -0400

    added in the dual dialog trigger as well as the ice dialog trigger

---
+++ Underwater/Assets/Scripts/Environment/DualDialogTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DualDialogTrigger : MonoBehaviour {
+
+    public TextAsset textFile;
+    public Player.PlayerControls desiredControl;
+    public List<GameObject> associatedObjects;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        PlayerHurtbox hurtbox = col.gameObject.GetComponent<PlayerHurtbox>();
+        if (hurtbox != null)
+        {
+            Player player = hurtbox.owner;
+
+            //Make the player go into a dialog state + cutscenes I guess
+            player.ActionFsm.ChangeState(new DialogState(player, player.ActionFsm, textFile, desiredControl));
+            foreach(GameObject obj in associatedObjects)
+            {
+                Destroy(obj);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Underwater/Assets/Scripts/Environment/IceDialogTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceDialogTrigger : MonoBehaviour {
+
+    public TextAsset textFile;
+    public Player.PlayerControls desiredControl;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        PlayerHurtbox hurtbox = col.gameObject.GetComponent<PlayerHurtbox>();
+        if (hurtbox != null && hurtbox.owner.weaponInventory.Find(x => x.name == "Ice") != null)
+        {
+            Player player = hurtbox.owner;
+
+            //Make the player go into a dialog state + cutscenes I guess
+            player.ActionFsm.ChangeState(new DialogState(player, player.ActionFsm, textFile, desiredControl));
+            Destroy(this.gameObject);
+        }
+    }
+}
