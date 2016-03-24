1643a502c04b68a82d753487f166442fbd66ce69
Your Name <you@example.com>
2016-03-23 22:13:25 -0400

    almost done

---
+++ Underwater/Assets/Scripts/Transitions/Credits.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public float breathingFadeoutTime;
+	private float timer;
+	public AudioSource breathing;
+	private float startingVolume;
+
+	void Start()
+	{
+		startingVolume = breathing.volume;
+		timer = breathingFadeoutTime;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		timer -= Time.deltaTime;
+
+		breathing.volume = startingVolume * timer / breathingFadeoutTime + 0.04f;
+
+
+		if (Controls.JumpInputDown()) {
+			//AutoFade.LoadLevel("Submarine", 3, 1, Color.black);
+			StartCoroutine(this.GetComponent<changeLevel>().change("mainMenu"));
+			timer = Mathf.Min(timer, 2.0f);
+		}
+	}
+}
