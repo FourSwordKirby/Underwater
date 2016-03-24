5f25fb4e2017a0d6fef08e6a3383187cad67ae42
Roger Liu <rogliu26@gmail.com>
2016-03-24 17:53:23 -0400

    added the skeleton and other prefabs

---
+++ Underwater/Assets/Scripts/Environment/AudioTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTrigger : MonoBehaviour {
+
+    public AudioClip audio;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        PlayerHurtbox hurtbox = col.gameObject.GetComponent<PlayerHurtbox>();
+        if (hurtbox != null)
+        {
+            if (audio != null)
+            {
+                AudioSource.PlayClipAtPoint(audio, this.transform.position);
+                audio = null;
+            }
+        }
+    }
+}
